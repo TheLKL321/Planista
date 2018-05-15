d33d839999a450bf21ab4479db3a1461e0dd479c
Łukasz Zarębski <lkl.zarebski@gmail.com>
2018-05-15 13:38:15 +0200

    RR

--- RR.java
+++ RR.java
@@ -1,7 +1,36 @@
 package com.thelkl;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class RR extends Strategy {
-    private int q;
+    private int q, currentQ;
 
+    public RR(LinkedList<Task> processesToDispense, ArrayList<Task> allProcesses, int q) {
+        super(new Dispenser(processesToDispense), allProcesses);
+        queuedTasks = new LinkedList<>();
+        this.q = q;
+        this.currentQ = q;
+    }
 
+    @Override
+    protected void handleTasks() {
+        if (currentQ == 0)
+            queuedTasks.add(queuedTasks.poll());
+
+        Task handledTask = queuedTasks.element();
+        handledTask.getHandled();
+        for (Task task : queuedTasks)
+            if (task.getId() != 1) task.waitInQueue();
+        if (handledTask.ifCompleted()) {
+            updateAverageTimes(queuedTasks.poll());
+            currentQ = q;
+        } else
+            this.currentQ--;
+    }
+
+    @Override
+    public String toString() {
+        return "RR-" + q;
+    }
 }
